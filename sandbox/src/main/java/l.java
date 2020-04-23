803bfd85bf5e694c124a67c001f3b6c8802cf251
Ronadier <kchlek@yandex.ru>
2020-04-23 18:28:09 +0300

    add package ru.learn.Chlek

--- sandbox/src/main/java/l.java
+++
@@ -1,6 +0,0 @@
-public class l {
-public static void main(String[] args) {
-
-	System.out.print("hello world!");
-}
-}
---
+++ sandbox/src/main/java/ru/learn/chlek/MyFirstProgram.java
@@ -0,0 +1,8 @@
+package ru.learn.chlek;
+
+public class MyFirstProgram {
+public static void main(String[] args) {
+
+	System.out.print("hello world1!");
+}
+}
