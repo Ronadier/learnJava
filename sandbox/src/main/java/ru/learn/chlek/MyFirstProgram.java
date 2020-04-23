48114b0d644fb4689ae51fa9cac2471cb8baf62a
Ronadier <kchlek@yandex.ru>
2020-04-23 21:40:40 +0300

    Homework 2

--- sandbox/src/main/java/ru/learn/chlek/MyFirstProgram.java
+++ sandbox/src/main/java/ru/learn/chlek/MyFirstProgram.java
@@ -7,11 +7,14 @@ public class MyFirstProgram {
 		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-		Point p = new Point(1, 9);
-		System.out.println("Расстояние между точками " + p.p1 + " и " + p.p2 + " = " + p.distance());
+		Point p1 = new Point(1, 2);
+		Point p2 = new Point(2, 1);
+
+		System.out.println("Расстояние между точками " + p1.x + "," + p1.y + " и " + p2.x + "," + p2.y + " = " + p1.distance(p1, p2));
 }
 	public static void hello (String somebody) {
 
 		System.out.println("hello " + somebody + "!");
 	}
+
 }
--- sandbox/src/main/java/ru/learn/chlek/Point.java
+++ sandbox/src/main/java/ru/learn/chlek/Point.java
@@ -1,13 +1,15 @@
 package ru.learn.chlek;
 
 public class Point {
-    public double p1;
-    public double p2;
-    public Point(double p1, double p2){
-        this.p1 = p1;
-        this.p2 = p2;
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
     }
-    public double distance (){
-        return Math.sqrt(p1*p2);
+    public double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
     }
+
 }
