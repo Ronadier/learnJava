031faea0e8ca0ba937a95a949000009cead355c5
Ronadier <kchlek@yandex.ru>
2020-04-23 19:11:07 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/learn/chlek/MyFirstProgram.java
+++ sandbox/src/main/java/ru/learn/chlek/MyFirstProgram.java
@@ -1,8 +1,22 @@
 package ru.learn.chlek;
 
 public class MyFirstProgram {
-public static void main(String[] args) {
-
-	System.out.print("hello world1!");
+	public static void main(String[] args) {
+		hello("world");
+		double len = 5;
+		System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
 }
+	public static void hello (String somebody) {
+
+		System.out.println("hello " + somebody + "!");
+	}
+	public static double area(double l){
+		return l * l;
+	}
+	public static double area(double a, double b){
+		return a * b;
+	}
 }
