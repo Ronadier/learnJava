affa32d9543fda4ab678e7b771f34fe439141c76
Ronadier <kchlek@yandex.ru>
2020-05-01 11:38:49 +0300

    Добавил возможность запуска в 3 браузерах

--- addressbook-web-tests/src/test/java/ru/chlek/learn/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/chlek/learn/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,9 @@ package ru.chlek.learn.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +16,20 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new ChromeDriver();
+        if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else  if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         contactHelper = new ContactHelper(wd);
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/chlek/learn/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/chlek/learn/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.chlek.learn.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.chlek.learn.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
